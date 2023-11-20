7627bd22470d70c769554c1bffc1edf749fbd8c0
Alfonso <alfonsobanzon@gmail.com>
2023-11-20 15:51:56 -0500

    rspec config

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_17_224031) do
+ActiveRecord::Schema.define(version: 2023_11_20_204550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,11 @@ ActiveRecord::Schema.define(version: 2023_11_17_224031) do
     t.string "email"
     t.string "password_digest"
     t.string "password_confirmation"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "widgets", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
