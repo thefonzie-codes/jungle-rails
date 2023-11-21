f3977b92df40647435ec207576f95e2f44c99524
Alfonso <alfonsobanzon@gmail.com>
2023-11-21 16:21:19 -0500

    fixed typo in config

--- config/initializers/cypress_rails.rb
+++ config/initializers/cypress_rails.rb
@@ -1,5 +1,5 @@
 require 'database_cleaner/active_record'
-Database_cleaner.strategy = :truncation
+DatabaseCleaner.strategy = :truncation
 
 return unless Rails.env.test?
 
@@ -12,7 +12,7 @@ CypressRails.hooks.after_transaction_start do
 end
 
 CypressRails.hooks.after_state_reset do 
-  
+
 end
 
 CypressRails.hooks.before_server_stop do
